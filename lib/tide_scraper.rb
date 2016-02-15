b02e0a63c4265bfda0c9273777716aae580c78e2
Martin Pugh <pugh@s3kr.it>
2016-02-15 23:52:00 +0000

    Add class method for returning next tide

--- lib/tide_scraper.rb
+++ lib/tide_scraper.rb
@@ -6,4 +6,15 @@ require 'pry'
 require 'date'
 
 module TideScraper
+  # Return the next prediction for a given list of tides
+  # (produced by get_prediction)
+  def self.get_next_tide(prediction)
+    next_tide = prediction[0]
+    prediction.each do |tide|
+      if tide[:time] < next_tide[:time]
+        next_tide = tide
+      end
+    end
+    return next_tide
+  end
 end
